86b6a9b28fbf02724146b3b5cc92041ac594b38b
GregFromTopFag <76189754+GregFromTopFag@users.noreply.github.com>
2020-12-17 20:12:38 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace RacismNigga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox23 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(449, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 174);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(58, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 179);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Get Ratted";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(309, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Status Never DTC NIGGER!";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(142, 333);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(471, 94);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 306);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Put Cheat Code here nigga";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(76, 20);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(496, 14);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(186, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "RACISM.CC FUCK GEORGE FLOYD";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(499, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(183, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "BOOT GREG OFFLINE";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(499, 66);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(183, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Kneel On A Nigga ";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(496, 93);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Rat Greg";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(483, 301);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(184, 20);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.Text = "CHARGE BACK ON GREG";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(13, 320);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.Text = "Auto Scam Indra";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(13, 356);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 12;
+            this.textBox9.Text = "Generate Bitcoin";
+            // 
+            // textBox10
+            // 
+            this.textBox10.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox10.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox10.Location = new System.Drawing.Point(1, 396);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(208, 20);
+            this.textBox10.TabIndex = 13;
+            this.textBox10.Text = "Send Dev Your Passowrds";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(646, 333);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(115, 20);
+            this.textBox11.TabIndex = 14;
+            this.textBox11.Text = "Send alex to mexic";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(646, 373);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 15;
+            this.textBox12.Text = "Generate Bypass";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(646, 406);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 16;
+            this.textBox13.Text = "Rat Smartie";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(212, 84);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 17;
+            this.textBox14.Text = "Rat RoseHacc";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(109, 20);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(100, 20);
+            this.textBox15.TabIndex = 18;
+            this.textBox15.Text = "DDOS DANNY";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(109, 14);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(716, 20);
+            this.textBox16.TabIndex = 18;
+            this.textBox16.Text = "DDOS DANNY";
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(12, 520);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(966, 20);
+            this.textBox17.TabIndex = 19;
+            this.textBox17.Text = "Quick Cum";
+            this.textBox17.TextChanged += new System.EventHandler(this.textBox17_TextChanged);
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(13, 561);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(965, 20);
+            this.textBox18.TabIndex = 20;
+            this.textBox18.Text = "Quick kneel";
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(13, 600);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(965, 20);
+            this.textBox19.TabIndex = 21;
+            this.textBox19.Text = "Fake Kneel";
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(12, 641);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(966, 20);
+            this.textBox20.TabIndex = 22;
+            this.textBox20.Text = "Seilent Neal";
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(13, 686);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(965, 20);
+            this.textBox21.TabIndex = 23;
+            this.textBox21.Text = "No Neal Through Walls";
+            // 
+            // textBox23
+            // 
+            this.textBox23.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox23.Location = new System.Drawing.Point(142, 508);
+            this.textBox23.Name = "textBox23";
+            this.textBox23.Size = new System.Drawing.Size(805, 20);
+            this.textBox23.TabIndex = 24;
+            this.textBox23.Text = "Nigga ESP";
+            this.textBox23.TextChanged += new System.EventHandler(this.textBox23_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1416, 743);
+            this.Controls.Add(this.textBox23);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox23;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RacismNigga
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox17_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox22_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox23_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
